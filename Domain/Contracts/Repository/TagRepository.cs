ccab8e60dff820cb35618c8aec4875e821894c30
Diego Godinho <diego.godinho@bs2.com>
2019-05-31 14:01:08 -0300

    First Check in

---
+++ Domain/Contracts/Repository/TagRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Domain.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contracts.Repository
+{
+    public interface ITagRepository : IRepository<Tag>
+    {
+        Task<List<Tag>> GetTasks(int userID);
+        Task<List<Tag>> GetTasksPaginated(RequestGrid request, int userID);
+    }
+}
---
+++ WebApplication4/Models/TagModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplication4.Models 
+{
+    public class TagModel 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Checked { get; set; }
+    }
+}
